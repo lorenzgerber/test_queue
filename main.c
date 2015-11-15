edd5199ccc46d1311a5c8de9b0aa4ec5517fbbca
Lorenz Gerber <genfys@gmail.com>
2015-11-15 23:23:43 +0100

    start to understand the dynamic memory allocation

--- main.c
+++ main.c
@@ -3,9 +3,26 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "queue.h"
 #include "list_2cell.h"
 
 int main (void){
+    // make a empty list
+    queue *que = queue_empty();
+    //queue_setMemHandler(que, free);
+    int val = 10;
+    int *value = malloc(sizeof(int));
+    *value = val;
+    queue_enqueue(que, value);
+    queue_dequeue(que);
+    if (queue_isEmpty(que)){
+        printf("TRUE, queue is empty");
+    }
+
+    // Check isEmpty(Empty)
+
+    // Check FALSE(Isempty(Enqueue(v, q))
+    queue_free(que);
     return 0;
 }
